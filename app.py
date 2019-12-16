fe933673d32e43e6b2e306d9843782645262647f
Gideon-Felt <gideon@feltslanding.com>
2019-12-16 11:23:03 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+from twilio.rest import Client
+import config
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
