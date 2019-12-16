56a1d0f2be8b8a7333b3cf0dbcd3ac340358924b
Gideon-Felt <gideon@feltslanding.com>
2019-12-16 12:13:15 -0700

    enabled twilio API and texting functionality with html form

--- app.py
+++ app.py
@@ -8,6 +8,24 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/send_sms', methods=["POST"])
+def send_sms():
+    phone_number = None
+    if request.method == 'POST':
+        phone_number = request.form['phone']
+        message = request.form['message']
+        account_sid = config.ACCOUNT_SID
+        auth_token = config.AUTH_TOKEN
+        client = Client(account_sid, auth_token)
+        message = client.messages \
+                .create(
+                    body= f"{message}",
+                    from_='+12564879671',
+                    to=f"+{phone_number}"
+                )
+        print(message.sid)
+        return render_template('message_sent.html')
+    return home()
 
 
 
